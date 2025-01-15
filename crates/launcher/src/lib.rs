ee6c5740093902e87513a67ba22bdd7001138136
ShellWen <me@shellwen.com>
2025-01-16 02:11:47 +0800

    :recycle: refactor windows launcher code

--- crates/launcher/src/lib.rs
+++ crates/launcher/src/lib.rs
@@ -22,6 +22,7 @@ fn launch_with_remote_thread_inject(executable: &str, args: &[&str], lib_path: &
     use tracing::*;
     use windows::core::PWSTR;
     use windows::core::{s, w};
+    use windows::Win32::Foundation::FARPROC;
     use windows::Win32::Foundation::STILL_ACTIVE;
     use windows::Win32::Foundation::TRUE;
     use windows::Win32::System::Diagnostics::Debug::WriteProcessMemory;
@@ -49,24 +50,24 @@ fn launch_with_remote_thread_inject(executable: &str, args: &[&str], lib_path: &
         len
     }
 
-    unsafe {
-        let args_str: String = format!(
-            "\"{}\" {}",
-            executable,
-            args.iter()
-                .map(|arg| arg.to_string())
-                // .map(|arg| format!("\"{}\"", arg))
-                .collect::<Vec<String>>()
-                .join(" ")
-        );
-        let mut args_utf16_vec = utf16_vec_from_str(args_str);
-        let args_pwstr = PWSTR::from_raw(args_utf16_vec.as_mut_ptr());
-        let mut path_utf16_vec = utf16_vec_from_str(lib_path.to_string());
-        let path_pwstr = PWSTR::from_raw(path_utf16_vec.as_mut_ptr());
-        let path_utf16_zeroend_size = get_pwstr_length(path_pwstr) * 2 + 2;
+    let args_str: String = format!(
+        "\"{}\" {}",
+        executable,
+        args.iter()
+            .map(|arg| arg.to_string())
+            // .map(|arg| format!("\"{}\"", arg))
+            .collect::<Vec<String>>()
+            .join(" ")
+    );
 
-        let mut process_info = PROCESS_INFORMATION::default();
-        info!("Creating process.");
+    let mut args_utf16_vec = utf16_vec_from_str(args_str);
+    let args_pwstr = PWSTR::from_raw(args_utf16_vec.as_mut_ptr());
+    let mut path_utf16_vec = utf16_vec_from_str(lib_path.to_string());
+    let path_pwstr = PWSTR::from_raw(path_utf16_vec.as_mut_ptr());
+    let path_utf16_zeroend_size = get_pwstr_length(path_pwstr) * 2 + 2;
+    let mut process_info = PROCESS_INFORMATION::default();
+    debug!("Creating process.");
+    unsafe {
         CreateProcessW(
             None,
             args_pwstr,
@@ -80,67 +81,84 @@ fn launch_with_remote_thread_inject(executable: &str, args: &[&str], lib_path: &
             &mut process_info,
         )
         .expect("CreateProcessW calling failed");
-        info!("PID: {}", process_info.dwProcessId);
-        info!("Alloc core lib path memory.");
-        let remote_memory = VirtualAllocEx(
+    }
+    info!("PID: {}", process_info.dwProcessId);
+    debug!("Alloc core lib path memory.");
+    let remote_lib_path_memory = unsafe {
+        VirtualAllocEx(
             process_info.hProcess,
             None,
             path_utf16_zeroend_size,
             MEM_COMMIT | MEM_RESERVE,
             PAGE_READWRITE,
-        );
-        assert!(!remote_memory.is_null());
-        info!("Remote lib path memory Address: {:p}.", remote_memory);
-        info!("Writing core lib path to process.");
+        )
+    };
+    assert!(!remote_lib_path_memory.is_null());
+    debug!(
+        "Remote lib path memory Address: {:p}.",
+        remote_lib_path_memory
+    );
+    debug!("Writing core lib path to process.");
+    unsafe {
         WriteProcessMemory(
             process_info.hProcess,
-            remote_memory,
+            remote_lib_path_memory,
             path_pwstr.0 as *const c_void,
             path_utf16_zeroend_size,
             None,
         )
         .expect("WriteProcessMemory called failed");
-        // let r_func_addr = unsafe{GetProcAddress(
-        //     GetModuleHandleA("kernel32.dll\0".as_ptr() as _),
-        //     "LoadLibraryW\0".as_ptr() as _,
-        // )};
-        info!("Getting LoadLibraryW address.");
-        let kernel_handle = GetModuleHandleW(w!("kernel32.dll")).unwrap();
-        let load_library_address =
-            (GetProcAddress(kernel_handle, s!("LoadLibraryW")).unwrap()) as *const c_void;
-        assert!(!load_library_address.is_null());
-        info!("Creating remote thread.");
-        let load_remote_thread_handle = CreateRemoteThread(
+    }
+
+    debug!("Getting LoadLibraryW address.");
+    let kernel_handle = unsafe { GetModuleHandleW(w!("kernel32.dll")) }.unwrap();
+    // it means FARPROC but with a value, equivalent to FARPROC.unwrap() when FARPROC has a value
+    #[allow(non_camel_case_types)]
+    type FARPROC_Value = unsafe extern "system" fn() -> isize;
+    let load_library_address: FARPROC_Value =
+        unsafe { GetProcAddress(kernel_handle, s!("LoadLibraryW")) }
+            .expect("GetProcAddress of LoadLibraryW failed");
+    debug!("Creating remote thread.");
+    let load_remote_thread_handle = unsafe {
+        CreateRemoteThread(
             process_info.hProcess,
             None,
             0,
             LPTHREAD_START_ROUTINE::from(std::mem::transmute::<
-                *const c_void,
+                FARPROC_Value,
                 unsafe extern "system" fn(*mut c_void) -> u32,
             >(load_library_address)),
-            Some(remote_memory),
+            Some(remote_lib_path_memory), // LoadLibraryW(lpLibFileName)
             0,
             None,
         )
-        .unwrap();
-        info!("Core lib inject success. Waiting for thread end.");
+    }
+    .unwrap();
+    info!("Core lib inject success. Waiting for thread end.");
+    unsafe {
         WaitForSingleObject(load_remote_thread_handle, INFINITE);
-        info!("Thread ended. Resume original thread.");
-        info!("--- Following is the original process output ---");
+    }
+    info!("Thread ended. Resume original thread.");
+    info!("--- Following is the original process output ---");
+    unsafe {
         ResumeThread(process_info.hThread);
+    }
+    unsafe {
         WaitForSingleObject(process_info.hProcess, INFINITE);
-        let mut exit_code: u32 = 0;
-        loop {
+    }
+    let mut exit_code: u32 = 0;
+    loop {
+        unsafe {
             GetExitCodeProcess(process_info.hProcess, &mut exit_code)
                 .expect("GetExitCodeProcess failed");
-            if exit_code != STILL_ACTIVE.0.into() {
-                break;
-            }
-            warn!("Process is still running even after WaitForSingleObject. This should not happen. Waiting for 500ms.");
-            sleep(Duration::from_millis(500)); // Why 500ms? Because I'm lazy
         }
-        ExitStatus::from_raw(exit_code)
+        if exit_code != STILL_ACTIVE.0.into() {
+            break;
+        }
+        warn!("Process is still running even after WaitForSingleObject. This should not happen. Waiting for 500ms.");
+        sleep(Duration::from_millis(500)); // Why 500ms? Because I'm lazy
     }
+    ExitStatus::from_raw(exit_code)
 }
 
 #[cfg(any(target_os = "linux", target_os = "macos"))]
